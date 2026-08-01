0879b6eb2590e03f04bf5eb2f869bfb7e945fff2
jcrc2208 <jcrc2208@gmail.com>
2026-08-01 09:39:02 -0600

    se inicio refactorizacion del back-end

--- back-end/api/dashboard.py
+++
@@ -1,8 +0,0 @@
-class Dashboard:
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
-
-def list_dashboards(self):
-    return list(self.dashboards.values())
